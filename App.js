1d3520c3fe852bce8ca8d6eb7c9aa1738f0f1a57
Ramazan KORKMAZ <rakodev@gmail.com>
2019-10-06 13:51:12 +0200

    header added

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Header from './components/Header';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Header title="Guess a number"></Header>
     </View>
   );
 }
@@ -12,8 +14,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
