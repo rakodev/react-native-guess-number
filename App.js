0eed84118b13558e6769211a59da4bf39d3ac4b8
Ramazan KORKMAZ <rakodev@gmail.com>
2019-10-29 13:11:36 +0100

    Text styling

--- App.js
+++ App.js
@@ -38,7 +38,6 @@ export default function App() {
   };
 
   let content = <StartGameScreen onStartGame={startGameHandler} />;
-  content = <GameOverScreen roundsNumber={1} userNumber={1} onRestart={configureNewGameHandler} />
   if (userNumber && guessRounds <= 0) {
     content = <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />;
   } else if (guessRounds > 0) {
--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import { View, StyleSheet, Button, Image } from 'react-native';
+import { View, StyleSheet, Button, Image, Text } from 'react-native';
 
 import BodyText from '../components/BodyText';
 import TitleText from "../components/TitleText";
+import Colors from '../constants/colors';
 
 const GameOverScreen = props => {
     return (
         <View style={styles.screen}>
             <TitleText>The Game is Over!</TitleText>
             <View style={styles.imageWrapper}>
-                {/* <Image source={require('../assets/img/success.png')} style={styles.image} resizeMode="cover" /> */}
-                <Image source={{ uri: 'https://abrahamswallet.com/wp-content/uploads/2017/12/samuel-ferrara-117219-1180x770.jpg' }} style={styles.image} resizeMode="cover" />
+                <Image source={require('../assets/img/success.png')} style={styles.image} resizeMode="cover" />
+                {/* <Image source={{ uri: 'https://abrahamswallet.com/wp-content/uploads/2017/12/samuel-ferrara-117219-1180x770.jpg' }} style={styles.image} resizeMode="cover" /> */}
             </View>
-            <BodyText>Number of Rounds: {props.roundsNumber}</BodyText>
-            <BodyText>Number was: {props.userNumber}</BodyText>
+            <View style={styles.resultTextContainer}>
+                <BodyText style={styles.resultText}>Your phone needed <Text style={styles.highlight}>{props.roundsNumber}</Text> rounds to guess the number <Text style={styles.highlight}>{props.userNumber}</Text></BodyText>
+            </View>
             <Button title="New Game" onPress="{props.onRestart}" />
         </View>
     );
@@ -37,6 +39,18 @@ const styles = StyleSheet.create({
     image: {
         width: '100%',
         height: '100%',
+    },
+    highlight: {
+        color: Colors.primary,
+        fontFamily: 'open-sans-bold',
+    },
+    resultText: {
+        textAlign: 'center',
+        fontSize: 20
+    },
+    resultTextContainer: {
+        marginHorizontal: 30,
+        marginVertical: 15,
     }
 });
 
